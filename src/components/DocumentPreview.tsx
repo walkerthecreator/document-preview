9d403d257fc109391d4a5668d0fbfe74d7c556e0
walkerthecreator <nitinjaswal2616@gmail.com>
2025-01-15 13:50:29 +0530

    remove buttons

--- src/components/DocumentPreview.tsx
+++ src/components/DocumentPreview.tsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState, useMemo, JSX } from "react";
 import * as pdfjsLib from "pdfjs-dist";
 import { features } from "../feature"
 import { Loading } from "./Loading";
-import { DownloadButton } from "./download"
-import { View } from "./View"
 
 type DocumentType = "pdf" | "image" | "word";
 
 export interface IDocumentPreview {
+    style?: React.CSSProperties;
     file?: File | null;
     url?: string;
     fit?: "cover" | "contain" | "fill" | "none" | "scale-down";
@@ -39,12 +38,12 @@ export const DocumentPreview = ({
     width = 300,
     height = 210,
     documentType,
+    style,
 }: IDocumentPreview): JSX.Element | null => {
     if (!url && !file) return null
     const [thumbnailUrl, setThumbnailUrl] = useState<string>("");
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
-    const [showButtons, setShowButtons] = useState(false);
     const { generatePdfThumbnail, generateWordThumbnail } = features;
 
     const altText = useMemo(
@@ -97,34 +96,14 @@ export const DocumentPreview = ({
 
     return (
         <div
-            onMouseOver={() => setShowButtons(true)}
-            onMouseOut={() => setShowButtons(false)}
-            style={styles.container(width, height)}>{
+            style={{ ...styles.container(width, height), ...style }}>
+            {
                 error ?
                     <div style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center", gap: 3 }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-x-2"><path d="M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /><path d="m8 12.5-5 5" /><path d="m3 12.5 5 5" /></svg>
                         {error}
                     </div> :
                     <>
-                        {
-                            !!url && showButtons &&
-                            <div className="fade-" style={{
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                flexDirection: "column",
-                                gap: 2,
-                                position: "absolute",
-                                width: "100%",
-                                height: "100%",
-                                zIndex: 10,
-                                borderRadius: 10,
-                                background: "rgba(0,0,0,0.1)",
-                            }}>
-                                <DownloadButton s3Url={url} fileName="pdf" type={documentType} />
-                                <View url={url} />
-                            </div>
-                        }
                         <div style={{
                             width: "100%",
                             height: "100%",
--- src/components/download.tsx
+++ src/components/download.tsx
@@ -15,6 +15,20 @@ export const DownloadButton: FC<{ s3Url: string; fileName: string; type: "pdf" |
   return (
     <span
       className='fade-up download-button'
+      style={{
+        background: "#333",
+        width: "25%",
+        minWidth: "fit",
+        padding: "4px 12px",
+        color: "white",
+        display: "flex",
+        gap: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 8,
+        fontSize: 12,
+        cursor: "pointer"
+    }} 
       onClick={handleDownload}>
       <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-cloud-download"><path d="M12 13v8l-4-4" /><path d="m12 21 4-4" /><path d="M4.393 15.269A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.436 8.284" /></svg>
       <span className='button-text'>Download</span>
